2f7a4f45478c2d51684bfe97d6f88892dc310604
RDAZ <RDAZ@RDAZ-PC>
2014-06-11 17:55:32 -0500

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<html>
+    <head>
+        
+    </head>
+    
+    <body>
+    
+    <?php
     echo 'hola mundo';
     echo 'hola roy y albert';
+    echo 'menu';
+    ?>
+        
+    </body>
+</html>
+    
 
-?>
+
